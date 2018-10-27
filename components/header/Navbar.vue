b8a0e250eeea66b3bd266c652658b39cb5be1af6
PhongPV <phamvanphong915@gmail.com>
2018-10-28 01:37:17 +0700

    Update Navbar.vue

--- components/header/Navbar.vue
+++ components/header/Navbar.vue
@@ -8,8 +8,8 @@
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">Product</a></li>
-        <li><a href="#">Support</a></li>
-        <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Service</a></li>
+        <li><a href="#">About Us</a></li>
         <li><a href="#">Cart</a></li>
         <li class="button-start"><a href="#">Getting started</a></li>
       </ul>
